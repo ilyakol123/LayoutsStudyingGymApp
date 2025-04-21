65c0c0299068b8194baf52c9da81011ae10329a9
Kolesnikov Ilya <ilja-kolesnikov@mail.ru>
2025-04-21 12:52:42 +0300

    Initial Commit

---
+++ LayoutsStudying/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LayoutsStudying
+//
+//  Created by Илья Колесников on 21.04.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LayoutsStudying/LayoutsStudyingApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutsStudyingApp.swift
+//  LayoutsStudying
+//
+//  Created by Илья Колесников on 21.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct LayoutsStudyingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
